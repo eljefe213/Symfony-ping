d5bd54f4c36988d0ba5e01e4280850814b2e35b3
Hamaz Abdelghani <101653284+eljefe213@users.noreply.github.com>
2026-02-12 20:18:10 +0100

    Use autowiring

--- src/Service/LoggerService.php
+++ src/Service/LoggerService.php
@@ -4,9 +4,12 @@ namespace App\Service;
 
 class LoggerService
 {
+    public function __construct(private string $projectDir)
+    {
+    }
     public function log(string $message): void
     {
-        $path = \dirname(__DIR__, 2) . '/var/custom.log';
+        $path = $this->projectDir . '/var/custom.log';
         file_put_contents($path, $message . PHP_EOL, FILE_APPEND);
     }
 }
