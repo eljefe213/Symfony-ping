2030bea88fa20a1c8422fd0a3b2e7580962d9a93
Hamaz Abdelghani <101653284+eljefe213@users.noreply.github.com>
2026-02-19 20:08:15 +0100

    Update mailer service & file transport mailer

--- src/Service/Mailer/FileTransport.php
+++ src/Service/Mailer/FileTransport.php
@@ -3,16 +3,35 @@
 namespace App\Service\Mailer;
 
 use App\Interface\MailerTransportInterface;
+use Psr\Log\LoggerInterface;
 
 class FileTransport implements MailerTransportInterface
 {
-    public function __construct(private string $projectDir)
-    {}
+    private string $dir;
+
+    public function __construct(
+        private string $projectDir,
+        private LoggerInterface $logger,
+    ) {
+        $this->dir = rtrim($this->projectDir, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . 'var' . DIRECTORY_SEPARATOR . 'email';
+    }
 
     public function send(string $email, string $subject, string $message): void
     {
-        $path = $this->projectDir . '/var/mails.log';
-        $line = sprintf("%s | %s | %s | %s\n", date('Y-m-d H:i:s'), $email, $subject, $message);
-        file_put_contents($path, $line, FILE_APPEND);
+        if (!is_dir($this->dir)) {
+            mkdir($this->dir, 0775, true);
+        }
+
+        $filename = $this->dir . DIRECTORY_SEPARATOR . date('Ymd_His') . '_' . bin2hex(random_bytes(4) . '.txt');
+
+        $payload = sprintf(
+            'TO : %s \nSUBJECT: %s\n\n%s\n',
+            $email,
+            $subject,
+            $message
+        );
+
+        file_put_contents($filename, $payload);
+        $this->logger->info('FileTransport wrote email',['file' => $filename]);
     }
 }
--- src/Service/Mailer/MailerService.php
+++ src/Service/Mailer/MailerService.php
@@ -3,14 +3,31 @@
 namespace App\Service\Mailer;
 
 use App\Interface\MailerTransportInterface;
+use Psr\Log\LoggerInterface;
 
 class MailerService
 {
-    public function __construct(private MailerTransportInterface $transport)
-    {}
+    public function __construct(
+        private LoggerInterface $logger,
+        private MailerTransportInterface $mailerTransport,
+        private string $adminEmail,
+    ) {}
 
-    public function welcome(string $email): void
+    public function welcome()
     {
-        $this->transport->send($email, 'Welcome', 'Hello and Welcome !');
+        $subject = 'Welcome';
+        $message = 'Bienvenue !';
+
+        $this->logger->info('MailerService.welcome start',[
+            'to' => $email,
+            'transport' => get_class($this->mailerTransport),
+        ]);
+
+        $this->mailerTransport->send($email, $subject, $message);
+
+        $this->logger->info('MailerService.welcome success ',[
+            'subject' => $subject,
+        ]);
     }
+
 }
